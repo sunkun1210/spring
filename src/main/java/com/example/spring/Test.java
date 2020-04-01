544844be598621701f4db09cbb66b5a8232647ef
sunkun <87983133@qq.com>
2020-04-01 19:24:48 +0800

    spring

--- src/main/java/com/example/spring/Test.java
+++ src/main/java/com/example/spring/Test.java
@@ -6,11 +6,11 @@ public class Test {
 
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext annotationConfigApplicationContext=new AnnotationConfigApplicationContext();
-        //把我们的这个类 注册到Spring的beanMap中去
-        annotationConfigApplicationContext.register(AppConfig.class);
-        //
-        annotationConfigApplicationContext.refresh();
+        AnnotationConfigApplicationContext annotationConfigApplicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+//        //把我们的这个类 注册到Spring的beanMap中去
+//        annotationConfigApplicationContext.register(AppConfig.class);
+//        //
+//        annotationConfigApplicationContext.refresh();
 
         System.out.println("========="+annotationConfigApplicationContext.getBean(User.class).hashCode());
         System.out.println("========="+annotationConfigApplicationContext.getBean(Application.class).hashCode());
