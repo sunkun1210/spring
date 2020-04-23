d89cf3830f9b2ee20f39d859128d407801f06e10
sunkun <87983133@qq.com>
2020-04-23 16:03:25 +0800

    归并排序

--- src/main/java/com/example/sort/MergeSort.java
+++ src/main/java/com/example/sort/MergeSort.java
@@ -3,13 +3,22 @@ package com.example.sort;
 public class MergeSort {
 
     public static void main(String[] args) {
-        int[] arr={1,4,7,8,3,6,9};
-        sort(arr);
-
+        int[] arr={1,4,100,7,10,2,3,5,8,9};
+        sort(arr,0,arr.length-1);
+        print(arr);
     }
 
-    static  void sort(int[] arr){
-        merge(arr,1,4,5);
+    static  void sort(int[] arr,int left,int right){
+        if (left>=right){
+            return;
+        }
+        //分成两半
+        int mid = (left+right)/2;
+        //左边排序
+        sort(arr,left,mid);
+        //右边排序
+        sort(arr,mid+1,right);
+        merge(arr,left,mid+1,right);
     }
 
     /***
@@ -35,6 +44,32 @@ public class MergeSort {
             temp[k++]=arr[i++];
         }
         while (j<=rightBound){
+            temp[k++] =arr[j++];
+        }
+     //   print(temp);
+        for (int m=0;m<temp.length;m++){
+            arr[leftPtr+m]=temp[m];
+        }
+    }
+
+
+    static void merge2(int[] arr){
+        int mid=arr.length/2;
+        int temp[] = new int[arr.length];
+        int i=0;
+        int j=mid+1;
+        int k=0;
+        while (i<=mid&&j<arr.length){
+            if (arr[i]<arr[j]){
+                temp[k++]=arr[i++];
+            }else{
+                temp[k++]=arr[j++];
+            }
+        }
+        while (i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while (j<arr.length){
             temp[k++] =arr[j++];
         }
         print(temp);
